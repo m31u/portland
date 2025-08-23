09d6cf74d21b196ce82fa81327712f1861b2067f
Matthew MAsters <mattdmasters@gmail.com>
2025-08-23 22:27:53 +0800

    Initial Commit

---
+++ portlandApp/portlandAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  portlandAppApp.swift
+//  portlandApp
+//
+//  Created by Matthew Masters on 8/23/25.
+//
+
+import SwiftUI
+
+@main
+struct portlandAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
